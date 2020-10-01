3f6c5918568a5047084320f625235e0c0bd98aa0
xiaohs <xiaohs@yamyamyam.net>
2020-10-01 10:58:54 +0800

    订单查询、修改个人中心样式布局

--- request/order.js
+++ request/order.js
@@ -3,11 +3,23 @@ import request from './request'
 //创建订单
 export function ordCreate(data) {
     const token = wx.getStorageSync('token');
-    console.log(token);
     return request({
         url: '/my/orders/create',
         method:'post',
         data,
+        headers:{
+            Authorization:token
+        }
+    })
+}
+
+//历史订单查询
+export function ordersAll(params) {
+    const token = wx.getStorageSync('token');
+    return request({
+        url: '/my/orders/all',
+        method:'get',
+        params,
         headers:{
             Authorization:token
         }
